348f12e99af0c5d72f3cb043525ee30b4072459d
camcam1604 <79933547+camcam1604@users.noreply.github.com>
2022-11-22 15:14:40 +0100

    Add files via upload

---
+++ js/Article1.js
@@ -0,0 +1,30 @@
+const  Article1 = {
+	template: `<div class="Article">
+        
+	<h1>Iron Man</h1>
+
+    <img src="">
+
+    <p>
+    Iron Man , considéré chez la plupart des fans de Marvels comme LE super héros.
+    Les films marvels nous ont dévoilé de nombreuses choses concernant L’histoire de Tony Stark, Iron Man, mais es ce que vous vous rappelez de tout ?
+    <br>
+    Anthony Edward, « Tony » Stark était un génie, milliardaire, playboy et philanthrope, ainsi que l'ancien PDG de Stark Industries, comme il le dis si bien.
+    Il utilisait sa richesse, son intelligence et génie hors du commun, et  joussait d'une vie de playboy durant des années au sein de son entreprise Stark Industries. Amis proche de James Rhones il finira capturé par les Dix Anneaux (Iron Man 1).
+    <br>
+    Sa vie étant en danger, Stark créa une armure blindée grâce a son intelligence hors norme, qu’il utilisa pour parvenir a bout de ses ennemis, c’est a partir de ce moment la que le héros appelé Iron Man arriva.
+    Il affrontera les terroristes ainsi que son ancien partenaire commercial Obadiha Stane qui essaya de le nuire (Iron man 2). En tant que playboy digne de ce nom, Stark décida Stark apprécia la renommée de sa nouvelle identité secrète et décida de la partager au monde entier, déclarant publiquement qu'il était Iron Man. 
+    <br>
+    Avec le travail de super héros, Stark se retrouva confronté à la mort à cause de l'empoissonnement de son corps par son propre Réacteur ARC, tandis qu'il fait face au terroriste revanchard Ivan Vanko, qui tente de détruire son héritage. Grâce à leurs aides respectives dans ces batailles, Stark accepte à contre-cœur de servir de consultant au Directeur Nick Fury de l'agence de maintien de la paix mondiale, le S.H.I.E.L.D., où il utilise sa position pour améliorer leurs technologies tandis qu'il entame une relation amoureuse avec Pepper Potts.
+    Le monde étant à nouveau menacé, Stark est convaincu pour rejoindre les Avengers et aider à vaincre les Chitauris et Loki. (Avengers)
+    Suite a ce combat acharné, il souffre de stress post-traumatique, l'amenant à créer l'Iron Legion (une série d'armures spéciales conçues par Iron Man reprenant le principe de son armure) pour garder la paix dans le monde et l'aider à se retirer.
+    Les menaces du Mandarin forcent Stark à quitter sa retraite pour protéger le monde, mais il mettra ses proches en danger et le laisse sans défense quand sa maison est détruite.
+    <br>
+    Stark continue tout de même sa mission, découvrant qu'Aldrich Killian est derrière les attaques et que Trevor Slattery était le prétendu Mandarin.
+    Finalement, Stark vainc Killian mais perd presque Pepper Potts durant l'opération. À la suite de la bataille, Stark détruit toutes ses armures avec le Protocole Ardoise Effacée. Toutefois, quand les Avengers se sont officiellement remobilisés à cause de la Guerre contre HYDRA, Stark continuera alors a protéger le monde sous sous armer et sous le nom dIron man.
+    </p>
+
+    <p> sources </p>
+    <a> lien source</a>
+
+</div>` }
