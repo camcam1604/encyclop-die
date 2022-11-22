8435b7b62fe44c9c21d3836b9f6ed23248eb045e
camcam1604 <79933547+camcam1604@users.noreply.github.com>
2022-11-22 15:15:41 +0100

    Add files via upload

---
+++ js/Article2.js
@@ -0,0 +1,24 @@
+const  Article2 = {
+	template: `<div class="Article">
+        
+	<h1>Les Avengers</h1>
+
+    <img src="">
+
+    <p>
+    Les Avengers, aussi connus sous le nom de Vengeurs, est une équipe d'individus extraordinaires, avec des super pouvoirs ou d'autres caractéristiques spéciales. 
+    <br>
+    Leur objectif est de protéger la stabilité mondiale des menaces intérieures ou extraterrestres. Bien que principalement affiliés aux États-Unis d'Amérique, ils ont travaillé avec les intérêts pacifiques du monde entier plutôt qu'un pays ou une organisation spécifique.
+    <br>
+    Les Avengers ont d'abord été assemblés par le S.H.I.E.L.D. quand Loki a envahi la Terre avec son armée Chitauri pour conquérir la planète. C’est lors de ce premier film que l’on retrouve Iron Man, Thor, Captain America, Black Widow, Hawkeye et Hulk.
+    Après la disparition du S.H.I.E.L.D. en raison de l'infiltration d'HYDRA, l'équipe n'était plus une division gouvernementale, devenant ainsi une organisation indépendante et privée. Stark Industries a transformé la tour Stark en tour des Avengers et l'équipe s'est jointe à la lutte contre HYDRA. 
+    <br>
+    Sous la direction de Captain America, l'équipe a mené à bien plusieurs opérations dans le monde entier avec succès. Avec le temps et les différents combats qu’ils ont menés, les Avengers ont surmonté de nombreuses épreuves.
+    <br>
+    Aujourd’hui bien des Avengers originels ne sont plus. Il faudra patienter pour observer leur nouvelle équipe.
+    </p>
+
+    <p> sources </p>
+    <a> lien source</a>
+
+</div>` }
